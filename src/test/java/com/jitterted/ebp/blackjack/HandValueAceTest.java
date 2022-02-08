54fd17200bd4f4fedd1e1007cf5786547fb5faf7
Ted M. Young <tedyoung@gmail.com>
2022-02-07 16:34:26 -0800

    LAB #7: Added boundary checking for Ace handling and fixed the bug that we discovered.

--- src/test/java/com/jitterted/ebp/blackjack/HandValueAceTest.java
+++ src/test/java/com/jitterted/ebp/blackjack/HandValueAceTest.java
@@ -2,6 +2,7 @@ package com.jitterted.ebp.blackjack;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
@@ -12,9 +13,7 @@ public class HandValueAceTest {
 
     @Test
     public void handWithOneAceTwoCardsIsValuedAt11() throws Exception {
-        List<Card> cards = List.of(new Card(DUMMY_SUIT, "A"),
-                                   new Card(DUMMY_SUIT, "5"));
-        Hand hand = Hand.createForTest(cards);
+        Hand hand = createHandWithRanksOf("A", "5");
 
         assertThat(hand.value())
                 .isEqualTo(11 + 5);
@@ -22,13 +21,33 @@ public class HandValueAceTest {
 
     @Test
     public void handWithOneAceAndOtherCardsEqualTo11IsValuedAt1() throws Exception {
-        List<Card> cards = List.of(new Card(DUMMY_SUIT, "A"),
-                                   new Card(DUMMY_SUIT, "8"),
-                                   new Card(DUMMY_SUIT, "3"));
-        Hand hand = Hand.createForTest(cards);
+        Hand hand = createHandWithRanksOf("A", "8", "3");
 
         assertThat(hand.value())
                 .isEqualTo(1 + 8 + 3);
     }
 
+    @Test
+    public void handWithOneAceAndOtherCardsValuedAt10IsValuedAt21() throws Exception {
+        Hand hand = createHandWithRanksOf("A", "4", "6");
+
+        assertThat(hand.value())
+                .isEqualTo(11 + 4 + 6);
+    }
+
+    @Test
+    public void handWithAceAndOtherCardsValuedAt12IsValuedAt13() throws Exception {
+        Hand hand = createHandWithRanksOf("A", "7", "5");
+
+        assertThat(hand.value())
+                .isEqualTo(1 + 7 + 5);
+    }
+
+    private Hand createHandWithRanksOf(String... ranks) {
+        List<Card> cards = new ArrayList<>();
+        for (String rank : ranks) {
+            cards.add(new Card(DUMMY_SUIT, rank));
+        }
+        return Hand.createForTest(cards);
+    }
 }
